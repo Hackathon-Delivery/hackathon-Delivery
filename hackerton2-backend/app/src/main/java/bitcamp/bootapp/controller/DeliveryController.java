43a3af7116c76b325233a9eeb670d7c9463ed2aa
unknown <sgw111@naver.com>
2023-01-16 12:07:03 +0900

    type: Update view
    view.html의 자바스크립트 부분 수정 (번호자리 자동입력되도록 수정)

--- hackerton2-backend/app/src/main/java/bitcamp/bootapp/controller/DeliveryController.java
+++ hackerton2-backend/app/src/main/java/bitcamp/bootapp/controller/DeliveryController.java
@@ -2,7 +2,7 @@ package bitcamp.bootapp.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
